1687a501e06c872c051051ccc79cd1cb076c203e
KingLeak95 <malikkalokoh2@gmail.com>
2023-11-04 09:27:42 -0400

    Connect to Postgress Locally

--- Makefile
+++ Makefile
@@ -1,4 +1,9 @@
 BINARY_NAME=to-do-list.out
+LOCAL_PG_CONTAINERNAME=todolist
+LOCAL_PG_USER=postgres
+LOCAL_PG_PASS=postgres
+LOCAL_PG_DBNAME=todolist
+LOCAL_PG_PORT=5432
 
 all: run
 
@@ -14,6 +19,18 @@ build:
 docker:
 	docker build -t to-do-list .
 
+postgres-start:
+	docker run -d --name "postgres-${LOCAL_PG_CONTAINERNAME}" \
+        -e POSTGRES_USER="${LOCAL_PG_USER}" \
+        -e POSTGRES_PASSWORD="${LOCAL_PG_PASS}" \
+        -e POSTGRES_DB="${LOCAL_PG_DBNAME}" \
+        -p "${LOCAL_PG_PORT}":5432 \
+        postgres >&2
+
+postgres-stop:
+	docker stop postgres-${LOCAL_PG_CONTAINERNAME} || true
+
 clean:
+	docker rm postgres-${LOCAL_PG_CONTAINERNAME} || true 
 	go clean
 	rm ${BINARY_NAME}
--- models/setup.go
+++ models/setup.go
@@ -1,20 +1,45 @@
 package models
 
 import (
-	"gorm.io/driver/sqlite"
+	"fmt"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
+type connection struct {
+	host     string 
+	dbname   string 
+	user     string
+	password string
+	port     int  
+}
+
+func NewDBConnection() *connection {
+	return &connection{
+		host:     "localhost",
+		dbname:   "todolist",
+		user:     "postgres",
+		password: "postgres",
+		port:     5432,
+	}
+}
+
 func ConnectDatabase() {
-	database, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	postgresConnection := NewDBConnection()
+	postgresDsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Asia/Shanghai",
+		postgresConnection.host,
+		postgresConnection.user,
+		postgresConnection.password,
+		postgresConnection.dbname,
+		postgresConnection.port)
+	database, err := gorm.Open(postgres.Open(postgresDsn), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect to database!")
 	}
 
 	err = database.AutoMigrate(&User{}, &Task{})
-
 	if err != nil {
 		return
 	}
